d7c802f844f8bc685c1289c1feba11da15e0a61b
jiangkn <jiangkn@work.com>
2024-07-18 15:04:20 +0800

    add options

---
+++ lua/options.lua
@@ -0,0 +1,10 @@
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+if vim.g.neovide then
+    -- Put anything you want to happen only in Neovide here
+    vim.g.neovide_cursor_vfx_mode = "railgun"
+    vim.g.neovide_transparency = 0.9
+end
